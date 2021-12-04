26b510c38b36550ef10a4c3aab22d73492dcdad2
Evans Owino <9787512+eowino@users.noreply.github.com>
2021-12-04 17:26:59 +0000

    day1 part 2

--- day-1/index.js
+++ day-1/index.js
@@ -7,8 +7,12 @@ const measurements = data.split('\n').map(Number);
 let increasingSum = 0;
 
 measurements.forEach((depth, index) => {
-    const prevDepth = measurements[index - 1];
-    if (prevDepth != null && depth > prevDepth) {
+    if (index === 0) return;
+
+    const currentSum = measurements[index - 1] + depth + measurements[index + 1];
+    const nextSum = depth + measurements[index + 1] + measurements[index + 2];
+
+    if (Number.isFinite(nextSum) && nextSum > currentSum) {
         increasingSum += 1
     }
 });
