cf9b3e8b947627de964eefa78ed6fc8d0bd40912
Evans Owino <9787512+eowino@users.noreply.github.com>
2021-12-06 23:08:21 +0000

    day 6 part A

--- day-6/partA.js
+++ day-6/partA.js
@@ -2,4 +2,25 @@
 
 // https://adventofcode.com/2021/day/6
 
-const data = require('fs').readFileSync('./data.txt', 'utf8').split('\n');
+const data = require('fs').readFileSync('./data.txt', 'utf8').split(',').map(Number);
+const state = [...data];
+let days = 0;
+
+const TOTAL_NUMBER_OF_DAYS = 80;
+
+while (days < TOTAL_NUMBER_OF_DAYS) {
+    const timersToPush = [];
+    state.forEach((internalTimer, index) => {
+        if (internalTimer === 0) {
+            state[index] = 6;
+            timersToPush.push(8);
+        } else {
+            state[index] -= 1;
+        }
+    });
+    
+    state.push(...timersToPush);
+    days += 1;
+}
+
+console.log('total fish:', state.length)
